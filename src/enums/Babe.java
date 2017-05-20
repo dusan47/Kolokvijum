44c35e3c81585870d1ee99a64445ca641d048be9
dusanpopovic <dusan47@hotmail.com>
2017-05-20 15:14:15 +0200

    drugikomit

--- src/enums/Babe.java
+++
@@ -1,5 +0,0 @@
-package enums;
-
-public enum Babe {
-BABA1,BABA2,BABA3,BABA4,BABA5;
-}
--- src/tracProgram/tracBuilder.java
+++ src/tracProgram/tracBuilder.java
@@ -1,11 +1,11 @@
 package tracProgram;
 
-import enums.Babe;
+
 import enums.Osoba;
 import enums.Trac;
 
 public interface tracBuilder {
-	void izaberiBabu(Babe baba);
+	
 	void izaberiOsobuX(Osoba osobaX);
 	void izaberiOsobuY(Osoba osobaY);
 	void izaberiTrac(Trac trac);
